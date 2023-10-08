e64ceade9e93e622a97ac9aba151143ba4cb8375
charles <k22009879@kcl.ac.uk>
2023-10-08 18:22:06 +0100

    limit attributes of model

--- things/models.py
+++ things/models.py
@@ -3,6 +3,6 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Thing(models.Model):
     name = models.CharField(max_length=30)
-    description = models.CharField(max_length=120)
+    description = models.CharField(max_length=120, blank=True)
     quantity = models.IntegerField(unique=False, validators=[MinValueValidator(0), MaxValueValidator(100)])
 # Create your models here.
